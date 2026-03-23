a6ccdd0d86df60e329de2097afeeaffb413ea7ba
Israel Tiburcio <agenor.it@gmail.com>
2026-03-23 12:23:52 -0600

    Initial Commit

---
+++ Kokoro/KokoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  KokoroApp.swift
+//  Kokoro
+//
+//  Created by Israel Tiburcio Suchil on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KokoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KokoroTests/KokoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  KokoroTests.swift
+//  KokoroTests
+//
+//  Created by Israel Tiburcio Suchil on 23/03/26.
+//
+
+import Testing
+@testable import Kokoro
+
+struct KokoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
